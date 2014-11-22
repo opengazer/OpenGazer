f4643224811091b8ebd74bbaeb8235686d737a59
Sam Dye <sam.re.dye@gmail.com>
2014-11-21 20:23:07 -0500

    Fix a couple small things in dependencies script

--- dependencies.sh
+++ dependencies.sh
@@ -13,19 +13,19 @@ echo "Installing dependencies for OpenGazer...this may take a while..."
 if [[ "$(uname)" == "Linux" ]] ; then
   echo "Installing libraries for OpenCV, GTK, Cairo, Boost, GSL, FANN and CMake"
   sudo apt-get update
-  sudo apt-get install -y libopencv-* libgtkmm-2.4-dev libcairomm-1.0-dev libboost-dev libgsl0-dev libfann2 libfann-dev libv4dl-dev cmake libboost-system-dev libboost-filesystem-dev wget
+  sudo apt-get install -y libopencv-dev libgtkmm-2.4-dev libcairomm-1.0-dev libboost-dev libboost-system-dev libboost-filesystem-dev libgsl0-dev libfann2 libfann-dev libv4l-dev cmake wget
 
   echo "Installing GSL from source"
-  cd ~
   wget ftp://ftp.gnu.org/gnu/gsl/gsl-1.15.tar.gz
   tar zxvf gsl-1.15.tar.gz
-  cd gsl-1.15
+  pushd gsl-1.15 2>&1 > /dev/null
   ./configure
   make
-  make install
+  sudo make install
 
   echo "Installing FFMPEG from source"
-  ./ffmpeg.sh
+  { popd; popd; } 2>&1 > /dev/null
+  bash ffmpeg.sh
 
 elif [[ "$(uname)" == "Darwin" ]] ; then
   echo "OSX detected - this script hasn't yet been tested on OSX, but it should be fine. Press any key to continue or kill it now with Ctrl-C..."
